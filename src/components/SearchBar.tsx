409f1441cd5a6d52dcdd72d24a1ea6ccd76583cc
ASHUTOSH-309 <ashutosh.sharma83020@gmail.com>
2024-07-30 15:55:10 +0530

    searchQuery made optional in SearchBar Component

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -22,7 +22,7 @@ type Props = {
     onSubmit: (formData: SearchForm) => void;
     placeHolder: string;
     onReset?: () => void;
-    searchQuery: string;
+    searchQuery?: string;
 }
 
 const SearchBar = ({ onSubmit, placeHolder, onReset, searchQuery }: Props) => {
